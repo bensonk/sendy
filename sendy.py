25bd87aa739d4e8e3410f3233af20d2ac8910f04
Benson Kalahar <bensonk@energysavvy.com>
2012-03-12 15:57:54 -0700

    Added commandline flags using argparse

--- sendy.py
+++ sendy.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
 from SocketServer import TCPServer
-from sys import argv
 
 def start_server(port):
+    print "HTTP server listening on {0}".format(port)
     httpd = TCPServer(("", port), Handler)
     httpd.serve_forever()
 
-def usage():
-    print "usage: sendy.py <port>"
-
 if __name__ == "__main__":
-    try:
-        port = int(argv[1])
-        start_server(port)
-    except:
-        usage()
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Start a tiny http server to share files')
+    parser.add_argument('-p', dest='port', metavar='port', default=4200, type=int)
+    parser.add_argument('-f', dest='file', metavar='file', nargs=1)
+    args = parser.parse_args()
+    if args.file:
+        print "This mode is not yet supported"
+    else:
+        start_server(args.port)
